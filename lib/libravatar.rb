4f4818113f78fae2851a3a50ad28de42df6c2e0e
Kang-min Liu <gugod@gugod.org>
2011-04-14 23:36:03 +0800

    generate from openid too

--- lib/libravatar.rb
+++ lib/libravatar.rb
@@ -8,15 +8,19 @@ class Libravatar
     x = URI.parse(s)
     x.host.downcase!
     x.scheme = x.scheme.downcase
+    if (x.path == "" && x.fragment == nil)
+      x.path = "/"
+    end
     return x.to_s
   end
 
   def initialize(options = {})
     @email = options[:email]
+    @openid = options[:openid]
   end
 
   def to_s
-    id = Digest::SHA2.hexdigest(@email)
+    id = Digest::SHA2.hexdigest(@email || self.class.normalize_openid(@openid))
     return "http://cdn.libravatar.org/avatar/" + id
   end
 end
--- test/test_libravatar.rb
+++ test/test_libravatar.rb
@@ -1,14 +1,24 @@
 require 'helper'
 
 class TestLibravatar < Test::Unit::TestCase
-  should "Generate url" do
+  should "Generate url from email" do
     # echo -n "user@example.com"|shasum -a 256
     # => b4c9a289323b21a01c3e940f150eb9b8c542587f1abfd8f0e1cc1ffc5e475514
     avatar = Libravatar.new(:email => "user@example.com")
     assert_equal avatar.to_s, "http://cdn.libravatar.org/avatar/b4c9a289323b21a01c3e940f150eb9b8c542587f1abfd8f0e1cc1ffc5e475514"
   end
 
+  should "Generate url from openid" do
+    avatar = Libravatar.new(:openid => "http://example.com/id/Bob")
+    assert_equal avatar.to_s, "http://cdn.libravatar.org/avatar/80cd0679bb52beac4d5d388c163016dbc5d3f30c262a4f539564236ca9d49ccd"
+
+    avatar = Libravatar.new(:openid => "hTTp://EXAMPLE.COM/id/Bob")
+    assert_equal avatar.to_s, "http://cdn.libravatar.org/avatar/80cd0679bb52beac4d5d388c163016dbc5d3f30c262a4f539564236ca9d49ccd"
+  end
+
   should "Normalize OpenID" do
     assert_equal Libravatar.normalize_openid("HTTP://EXAMPLE.COM/id/Bob"), "http://example.com/id/Bob"
+
+    assert_equal Libravatar.normalize_openid("HTTP://EXAMPLE.COM"), "http://example.com/"
   end
 end
