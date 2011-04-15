b886f76d0ee70ffce2a9870d8a3276f1f93f8063
Kang-min Liu <gugod@gugod.org>
2011-04-15 10:22:42 +0800

    accept size and default options

--- lib/libravatar.rb
+++ lib/libravatar.rb
@@ -13,22 +13,35 @@ require 'digest/sha2'
 require 'uri'
 
 class Libravatar
-  attr_accessor :size, :email, :openid, :size
+  attr_accessor :email, :openid, :size, :default
 
   # The options should contain :email or :openid values.  If both are
   # given, email will be used. The value of openid and email will be
   # normalized by the rule described in http://www.libravatar.org/api
+  #
+  # List of option keys:
+  #
+  # - :email
+  # - :openid
+  # - :size An integer ranged 1 - 512, default is 80.
+  #
   def initialize(options = {})
-    @email  = options[:email]
-    @openid = options[:openid]
-    @size   = options[:size]
+    @email   = options[:email]
+    @openid  = options[:openid]
+    @size    = options[:size]
+    @default = options[:default]
   end
 
   # Generate the libravatar URL
   def to_s
     @email.downcase! if @email
     id = Digest::SHA2.hexdigest(@email || normalize_openid(@openid))
-    return "http://cdn.libravatar.org/avatar/" + id
+    s  = @size ? "s=#{@size}" : nil
+    d  = @default ? "d=#{@default}" : nil
+
+    query = [s,d].reject{|x|!x}.join("&")
+    query = "?#{query}" unless query == ""
+    return "http://cdn.libravatar.org/avatar/" + id + query
   end
 
   private
--- test/test_libravatar.rb
+++ test/test_libravatar.rb
@@ -8,6 +8,10 @@ class TestLibravatar < Test::Unit::TestCase
     assert_equal avatar.to_s, "http://cdn.libravatar.org/avatar/b4c9a289323b21a01c3e940f150eb9b8c542587f1abfd8f0e1cc1ffc5e475514"
 
     assert_equal Libravatar.new(:email => "USER@ExAmPlE.CoM").to_s, "http://cdn.libravatar.org/avatar/b4c9a289323b21a01c3e940f150eb9b8c542587f1abfd8f0e1cc1ffc5e475514"
+
+    assert_equal Libravatar.new(:email => "USER@ExAmPlE.CoM", :default => "http://example.com/avatar.png").to_s, "http://cdn.libravatar.org/avatar/b4c9a289323b21a01c3e940f150eb9b8c542587f1abfd8f0e1cc1ffc5e475514?d=http://example.com/avatar.png"
+
+    assert_equal Libravatar.new(:email => "USER@ExAmPlE.CoM", :size => 512, :default => "mm").to_s, "http://cdn.libravatar.org/avatar/b4c9a289323b21a01c3e940f150eb9b8c542587f1abfd8f0e1cc1ffc5e475514?s=512&d=mm"
   end
 
   should "Generate url from openid" do
@@ -16,6 +20,9 @@ class TestLibravatar < Test::Unit::TestCase
 
     avatar = Libravatar.new(:openid => "hTTp://EXAMPLE.COM/id/Bob")
     assert_equal avatar.to_s, "http://cdn.libravatar.org/avatar/80cd0679bb52beac4d5d388c163016dbc5d3f30c262a4f539564236ca9d49ccd"
+
+    avatar = Libravatar.new(:openid => "hTTp://EXAMPLE.COM/id/Bob", :size => 512)
+    assert_equal avatar.to_s, "http://cdn.libravatar.org/avatar/80cd0679bb52beac4d5d388c163016dbc5d3f30c262a4f539564236ca9d49ccd?s=512"
   end
 
   should "Normalize OpenID" do
